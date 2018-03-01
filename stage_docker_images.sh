49c4527c10cdb1d4fa504eaa80228cced2e6fe8b
Alex Sparrow <alex@quartic.io>
2018-03-01 16:24:52 +0000

    Add prometheus node-exporter (#108)
    * Add prometheus node-exporter
    
    * Spacing
    
    * Spacing

--- stage_docker_images.sh
+++ stage_docker_images.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -ex
 docker pull registry
+docker pull quay.io/prometheus/node-exporter
+docker tag quay.io/prometheus/node-exporter node-exporter
 
 docker save \
     registry \
+    node-exporter \
     fnanny:273c0f1e56 \
     toolbelt:273c0f1e56 \
     prometheus:273c0f1e56-prometheus.e9cd0b5 \
