b0f4505588b6307e322689c5b5d108330ab61cbb
Alex Sparrow <alex@quartic.io>
2018-03-06 10:24:52 +0000

    [SOL-1098] spatial local cluster Poland 100k & changes to ansible (#115)
    * Poland 100k
    
    * Remove trailing ws
    
    * Remove ulimits
    
    * Extract variable
    
    * Refactor script

--- stage_docker_images.sh
+++ stage_docker_images.sh
@@ -4,10 +4,11 @@ docker pull registry
 docker pull quay.io/prometheus/node-exporter
 docker tag quay.io/prometheus/node-exporter node-exporter
 
+PLATFORM_VERSION=273c0f1e56
 docker save \
     registry \
     node-exporter \
-    fnanny:17b8242f39 \
-    toolbelt:17b8242f39 \
-    prometheus:17b8242f39-prometheus.e9cd0b5 \
+    fnanny:$PLATFORM_VERSION \
+    toolbelt:$PLATFORM_VERSION \
+    prometheus:$PLATFORM_VERSION-prometheus.e9cd0b5 \
     -o roles/docker_registry/files/repository.tar
