c9c851a465442e2baaed3ed3410f176e142c85fe
christophergatto <37303370+christophergatto@users.noreply.github.com>
2018-04-24 12:13:23 +0100

    SOL-1255: initial pathfinding framework (#154)
    * Initial schema changes for pathfinding
    
    * Untested AStar
    
    * Unit Test for 3 node graph
    
    * Made A* more readable
    
    * Backing out accidental VehicleBehaviour commit
    
    * Breaking out to multiple files
    
    * Using QuickGraphs as an algorithm and graph library. Creating an initial framework for pathfinding
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup of package.config
    
    * Unit test is now 5 nodes and circular
    
    * Added some descriptive comments
    
    * No longer returning lambda for trygetpath:
    
    * Moving VehicleRouting to the Vehicles worker project
    
    * Removed debug code from test, renamed test
    
    * Using CoordinateExtension to calculate 3D distsnce
    
    * Adding unit tests based on the sanfran-small snapshot
    
    * Unit test now computes all strongly connected components in the graph and selects the two nodes closest to the NE and SW boundaries of the world. A route is computed from one node to the other, both ways
    
    * Savings nodes and edges to reduce number of graph rebuilds
    
    * Checking to make sure map has not been rebuilt
    
    * Renaming test to something more specific
    
    * Storing nodes and edges hash instead of lists, saves space
    
    * Added test to validate SCCs. Reduced map size to 5000 units squared. Extractred utilities to PathfinderUtils
    
    * Cleanup
    
    * Reducing repetitious route calculation in Test_Inter_SCC_Traversal
    
    * Factoring out common code from SCCs tests, making them more readable
    
    * Broke out Graph loading utilites to a seperate file

---
+++ Pathfinding/Pathfinder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using QuickGraph;
+using QuickGraph.Algorithms.Observers;
+using QuickGraph.Algorithms.ShortestPath;
+
+namespace Shared.Pathfinding
+{
+    /// <summary>
+    /// Small wrapper for QuickGraph ShortestPathAlgorithmBase that enables clients to
+    /// retrieve a shortest path as an IEnumerable<TEdge>
+    /// </summary>
+    /// <typeparam name="TVertex">Generic Node Type</typeparam>
+    /// <typeparam name="TEdge">Generic Edge Type that implements QuikcGraph IEdge<TVertex></typeparam>
+    public class Pathfinder<TVertex, TEdge> where TEdge : IEdge<TVertex>
+    {
+        private readonly ShortestPathAlgorithmBase<TVertex, TEdge, IVertexListGraph<TVertex, TEdge>> _algorithm;
+
+        public Pathfinder(ShortestPathAlgorithmBase<TVertex, TEdge, IVertexListGraph<TVertex, TEdge>> algorithm)
+        {
+            _algorithm = algorithm;
+        }
+
+        /// <summary>
+        /// Computes the algorithm with an attached Vertex Predecessor Recorder to enable pathfinding
+        /// </summary>
+        /// <returns>TryFunc that queries the recorderObserver for a path</returns>
+        public IEnumerable<TEdge> FindPath(TVertex destination)
+        {
+            var recorderObserver = new VertexPredecessorRecorderObserver<TVertex, TEdge>();
+
+            recorderObserver.Attach(_algorithm);
+
+            _algorithm.Compute();
+
+            var predecessors = recorderObserver.VertexPredecessors;
+
+            if (predecessors.TryGetPath(destination, out var edges))
+            {
+                return edges;
+            }
+            throw new Exception("No path found.");
+        }
+    }
+}
