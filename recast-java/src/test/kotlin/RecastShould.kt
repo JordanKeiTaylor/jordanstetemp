69eee5a15551ca37278749a125a21f1c8b5ff34e
Alex Sparrow <alexsparrow@improbable.io>
2018-05-22 17:22:54 +0100

    Reorganise tests

--- recast-java/src/test/kotlin/RecastShould.kt
+++ recast-java/src/test/kotlin/RecastShould.kt
@@ -2,70 +2,83 @@ import com.sun.jna.Native
 import io.improbable.ste.recast.RcConfig
 import io.improbable.ste.recast.RecastLibrary
 import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import java.io.File
 
 class RecastShould {
     @Test
+    fun create_a_context() {
+        val recast = loadLibrary()
+        val ctx = recast.rcContext_create()
+        assertThat(ctx, notNullValue())
+        recast.rcContext_delete(ctx!!)
+    }
+
+    @Test
     fun create_a_navmesh() {
-        val INSTANCE = Native.loadLibrary("recastwrapper", RecastLibrary::class.java) as RecastLibrary
+        val recast = loadLibrary()
+        val ctx = recast.rcContext_create()
+        val config = createConfig()
 
-        val url = this.javaClass.getResource("Tile_+007_+006_L21.obj")
-        val ctx = INSTANCE.rcContext_create()
-        val mesh = INSTANCE.load_mesh(ctx!!, File(url.toURI()).absolutePath)
+        val mesh = recast.load_mesh(ctx!!, terrainTilePath())
+        assertThat(mesh, notNullValue())
 
-        val cellSize = 0.3
-        val cellHeight = 0.2
-        val agentHeight = 2.0
-        val agentRadius = 0.6
-        val agentMaxClimb = 20
-        val agentMaxSlope = 45.0
-        val regionMinSize = 8
-        val regionMergeSize = 20
-        val edgeMaxLen = 12.0
-        val edgeMaxError = 1.3
-        val vertsPerPoly = 6.0
-        val detailSampleDist = 6.0
-        val detailSampleMaxError = 1.0
-        val tileSize = 32
+        val chf = recast.compact_heightfield_create(ctx!!, config, mesh!!)!!
+        val polymesh = recast.polymesh_create(ctx!!, config, chf!!)!!
+        val polyMeshDetail = recast.polymesh_detail_create(ctx!!, config, polymesh, chf)!!
+        val navMeshDataResult = recast.navmesh_data_create(ctx!!, config, polyMeshDetail!!, polymesh, mesh, 0, 0, Constants.agentHeight.toFloat(), Constants.agentRadius.toFloat(), Constants.agentMaxClimb.toFloat())
+        assertThat(navMeshDataResult!!.size, equalTo(2248))
+
+        recast.rcContext_delete(ctx!!)
+    }
+
+    private fun loadLibrary() = Native.loadLibrary("recastwrapper", RecastLibrary::class.java) as RecastLibrary
+
+    private fun terrainTilePath() = File(this.javaClass.getResource("Tile_+007_+006_L21.obj").toURI()).absolutePath
+
+    private fun createConfig() = RcConfig.ByReference().apply {
+        width = Constants.tileSize + 2 * Constants.borderSize
+        height = Constants.tileSize + 2 * Constants.borderSize
+        tileSize = Constants.tileSize
+        borderSize = Constants.borderSize
+        cs = Constants.cellSize.toFloat()
+        ch = Constants.cellHeight.toFloat()
+        walkableSlopeAngle = Constants.agentMaxSlope.toFloat()
+        walkableHeight = Math.ceil(Constants.agentHeight / Constants.cellHeight).toInt()
+        walkableClimb = Math.ceil(Constants.agentMaxClimb / Constants.cellHeight).toInt()
+        walkableRadius = Constants.walkableRadius
+        maxEdgeLen = (Constants.edgeMaxLen / Constants.cellSize).toInt()
+        maxSimplificationError = Constants.edgeMaxError.toFloat()
+        minRegionArea = Constants.regionMinSize * Constants.regionMinSize
+        mergeRegionArea = Constants.regionMergeSize * Constants.regionMergeSize
+        maxVertsPerPoly = Constants.vertsPerPoly.toInt()
+        detailSampleDist = if (Constants.detailSampleDist < 0.9) {
+            0.0f
+        } else {
+            (Constants.cellSize * Constants.detailSampleDist).toFloat()
+        }
+        detailSampleMaxError = (Constants.cellHeight * Constants.detailSampleMaxError).toFloat()
+    }
+
+    object Constants {
+        const val cellSize = 0.3
+        const val cellHeight = 0.2
+        const val agentHeight = 2.0
+        const val agentRadius = 0.6
+        const val agentMaxClimb = 20
+        const val agentMaxSlope = 45.0
+        const val regionMinSize = 8
+        const val regionMergeSize = 20
+        const val edgeMaxLen = 12.0
+        const val edgeMaxError = 1.3
+        const val vertsPerPoly = 6.0
+        const val detailSampleDist = 6.0
+        const val detailSampleMaxError = 1.0
+        const val tileSize = 32
 
         val walkableRadius = Math.ceil((agentRadius / cellSize)).toInt()
         val borderSize = walkableRadius + 3
-
-        val config = RcConfig.ByReference().apply {
-            width = tileSize + 2 * borderSize
-            height = tileSize + 2 * borderSize
-            this.tileSize = tileSize
-            this.borderSize = borderSize
-            cs = cellSize.toFloat()
-            ch = cellHeight.toFloat()
-            bmin = floatArrayOf(0.0f, 0.0f, 0.0f)
-            bmax = floatArrayOf(0.0f, 0.0f, 0.0f)
-            walkableSlopeAngle = agentMaxSlope.toFloat()
-            walkableHeight = Math.ceil(agentHeight / cellHeight).toInt()
-            walkableClimb = Math.ceil(agentMaxClimb / cellHeight).toInt()
-            this.walkableRadius = walkableRadius
-            maxEdgeLen = (edgeMaxLen / cellSize).toInt()
-            maxSimplificationError = edgeMaxError.toFloat()
-            minRegionArea = regionMinSize * regionMinSize
-            mergeRegionArea = regionMergeSize * regionMergeSize
-            maxVertsPerPoly = vertsPerPoly.toInt()
-            this.detailSampleDist = if (detailSampleDist < 0.9) {
-                0.0f
-            } else {
-                (cellSize * detailSampleDist).toFloat()
-            }
-            this.detailSampleMaxError = (cellHeight * detailSampleMaxError).toFloat()
-        }
-
-        val chf = INSTANCE.compact_heightfield_create(ctx!!, config, mesh!!)!!
-        val polymesh = INSTANCE.polymesh_create(ctx!!, config, chf!!)!!
-        val polyMeshDetail = INSTANCE.polymesh_detail_create(ctx!!, config, polymesh, chf)!!
-
-        val navMeshDataResult = INSTANCE.navmesh_data_create(ctx!!, config, polyMeshDetail!!, polymesh, mesh, 0, 0, agentHeight.toFloat(), agentRadius.toFloat(), agentMaxClimb.toFloat())
-        assertThat(navMeshDataResult!!.size, equalTo(2248))
-
-        INSTANCE.rcContext_delete(ctx!!)
     }
 }
