bca05c1448a1270587d0648025e38d5aa7568847
Alex Sparrow <alexsparrow@improbable.io>
2018-05-31 15:30:47 +0100

    find many paths

--- recast-csharp/Recast.Tests/DetourShould.cs
+++ recast-csharp/Recast.Tests/DetourShould.cs
@@ -50,5 +50,33 @@ namespace Recast.Tests
                 Assert.IsNotNull(result);
             }
         }
+        
+        [Test]
+        public void find_many_paths()
+        {
+            using (var ctx = new RecastContext())
+            {
+                var config = Constants.createDefaultConfig();
+                var mesh = ctx.LoadInputGeom("./Resources/Tile_+007_+006_L21.obj", true);
+                ctx.CalcGridSize(ref config, mesh);
+                var chf = ctx.CreateCompactHeightfield(config, mesh);
+                var polyMesh = ctx.CreatePolyMesh(config, chf);
+                var polyMeshDetail = ctx.CreatePolyMeshDetail(config, polyMesh, chf);
+                var navMeshData = ctx.CreateNavMeshData(config, polyMeshDetail, polyMesh, mesh, 0, 0,
+                    Constants.agentHeight, Constants.agentRadius, Constants.agentMaxClimb);
+                var navMesh = ctx.CreateNavMesh(navMeshData);
+                var navMeshQuery = ctx.CreateNavMeshQuery(navMesh);
+
+                for (int i = 0; i < 10000; i++)
+                {
+
+                    var pointA = ctx.FindRandomPoint(navMeshQuery);
+                    var pointB = ctx.FindRandomPoint(navMeshQuery);
+
+                    var result = ctx.FindPath(navMeshQuery, pointA, pointB);
+                    Assert.IsNotNull(result);
+                }
+            }
+        }
     }
 }
