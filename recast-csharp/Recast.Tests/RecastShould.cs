70d69a494503067e58cce3282340065f0a4d6494
Alex Sparrow <alexsparrow@improbable.io>
2018-05-31 13:58:51 +0100

    Move tests

--- recast-csharp/Recast.Tests/RecastShould.cs
+++ recast-csharp/Recast.Tests/RecastShould.cs
@@ -124,27 +124,5 @@ namespace Recast.Tests
                 Assert.IsNotNull(navMeshQuery);
             }
         }
-        
-        [Test]
-        public void find_random_point()
-        {
-            using (var ctx = new RecastContext())
-            {
-                var config = Constants.createDefaultConfig();
-                var mesh = ctx.LoadInputGeom("./Resources/Tile_+007_+006_L21.obj", true);
-                ctx.CalcGridSize(ref config, mesh);
-                var chf = ctx.CreateCompactHeightfield(config, mesh);
-                var polyMesh = ctx.CreatePolyMesh(config, chf);
-                var polyMeshDetail = ctx.CreatePolyMeshDetail(config, polyMesh, chf);
-                var navMeshData = ctx.CreateNavMeshData(config, polyMeshDetail, polyMesh, mesh, 0, 0,
-                    Constants.agentHeight, Constants.agentRadius, Constants.agentMaxClimb);
-                var navMesh = ctx.CreateNavMesh(navMeshData);
-                var navMeshQuery = ctx.CreateNavMeshQuery(navMesh);
-                var result = ctx.FindRandomPoint(navMeshQuery);
-                Assert.IsNotNull(result);
-                Assert.IsTrue((result.status & (1u << 30)) != 0);
-                Assert.AreEqual(result.point.Length, 3);
-            }
-        }
     }
 }
