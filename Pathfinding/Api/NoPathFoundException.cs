05c50ac34bccb25148a737cd2f1b6dd1752734dc
christophergatto <37303370+christophergatto@users.noreply.github.com>
2018-06-11 23:14:39 -0400

    Spike/pathfinding library prototype (#164)
    * Initial commit
    
    * Still debugging odd teleport behavior
    
    * Vehicles no longer die at origin
    
    * Some cleanup
    
    * Removing old pathfinder subproject
    
    * No longer bombarded with Pathfinder logs
    
    * More cleanup, graph and points csv are now run parameters
    
    * No longer storing the graphs as worker resources
    
    * Cleanup
    
    * Moved some utils out of Environment, tests for NavGraph Pathfinding API
    
    * Changes to appease the stylecop gods (we should remove stylecop)
    
    * Path changed to PathResult, contains error information for failed paths
    
    * Cleanup
    
    * Added PlayerSpawner back to Startup
    
    * Cleanup
    
    * Fixing TC build
    
    * Adding worker sdk download to Snapshots.csproj
    
    * Adding default csv to worker
    
    * Revert "Adding worker sdk download to Snapshots.csproj"
    
    This reverts commit b162ebccb67bb34036f1bf0d82c6f42a5bb4f227.
    
    * Revert "Fixing TC build"
    
    This reverts commit 612b12f8fe9f58cddd87b3a3e3e07f681f004096.
    
    * Fixing build
    
    * Fixing TC
    
    * Added graph csvs to resources

---
+++ Pathfinding/Api/NoPathFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shared.Pathfinding.Api
+{
+    public class NoPathFoundException : Exception
+    {
+        public NoPathFoundException(string message)
+            : base(message)
+        {
+        }
+    }
+}
