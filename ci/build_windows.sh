c08f2c67b7932f52b28ce028982f3df96661228f
Alex Sparrow <alexsparrow@improbable.io>
2018-06-22 15:55:42 +0100

    Better

--- ci/build_windows.sh
+++ ci/build_windows.sh
@@ -2,7 +2,7 @@
 
 cd recast-wrapper
 export PATH="$PWD/cmake-3.12.0-rc1-win64-x64/bin;$PATH"
-echo "PATH is: %%path%%"
+echo "PATH is: $PATH"
 echo
 
 echo "Fetching cmake 3.12"
@@ -10,7 +10,6 @@ wget -q https://cmake.org/files/v3.12/cmake-3.12.0-rc1-win64-x64.zip
 unzip -q cmake-3.12.0-rc1-win64-x64.zip
 echo
 
-echo "CMake version"
 cmake --version
 echo
 
