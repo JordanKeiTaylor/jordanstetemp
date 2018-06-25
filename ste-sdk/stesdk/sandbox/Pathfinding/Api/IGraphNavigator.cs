2f8b494f7fd8bdf3cb313c2061e0d61826871fe2
christophergatto <christophergatto@improbable.io>
2018-06-25 11:33:35 +0100

    Splitting out graph and mesh APIs

---
+++ ste-sdk/stesdk/sandbox/Pathfinding/Api/IGraphNavigator.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Improbable.Sandbox.Pathfinding.Api
+{
+    public class IGraphNavigator
+    {
+        Task<PathResult> GetGraphPath(PathNode start, PathNode stop, Mobility mobility = null);
+    }
+}
---
+++ ste-sdk/stesdk/sandbox/Pathfinding/Api/IMeshNavigator.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Improbable.Sandbox.Pathfinding.Api
+{
+    public class IMeshNavigator
+    {
+        Task<PathResult> GetMeshPath(PathNode start, PathNode stop, Mobility mobility = null);
+    }
+}
