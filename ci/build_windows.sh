a1d235fbd802405dff28d7ed944a8e162aee2e92
Alex Sparrow <alexsparrow@improbable.io>
2018-06-25 12:24:03 +0100

    export

--- ci/build_windows.sh
+++ ci/build_windows.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-ls "C:/Windows/Microsoft.Net/Framework/v4.0.30319/MSBuild/15.0/Bin/msbuild.exe"
-export MSBUILD_DIR="C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/MSBuild/15.0/Bin/"
+#export MSBUILD_DIR="C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/MSBuild/15.0/Bin/"
 ls "$MSBUILD_DIR"
 
 cd recast-wrapper
