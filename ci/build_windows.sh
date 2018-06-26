3f0c78b7efe83885f9ee54032936819cdc26baa2
Alex Sparrow <alexsparrow@improbable.io>
2018-06-26 10:43:51 +0100

    Do some pre-flight checks

--- ci/build_windows.sh
+++ ci/build_windows.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+set -eux
+
+# Preflight checks
+make --version
+gcc --version
+g++ --version
 
 export MSBUILD_DIR="C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/MSBuild/15.0/Bin/"
+export PATH="$PWD/cmake-3.12.0-rc1-win64-x64/bin;$PATH"
 
 cd recast-wrapper
-export PATH="$PWD/cmake-3.12.0-rc1-win64-x64/bin;$PATH"
 echo "PATH is: $PATH"
 echo
 
