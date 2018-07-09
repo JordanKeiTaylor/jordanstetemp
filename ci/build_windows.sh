5c0d2699dc20264bddbcc15bc91e825fef2b22f6
Alex Sparrow <alexsparrow@improbable.io>
2018-07-09 16:39:44 +0100

    Rejigger ci

--- ci/build_windows.sh
+++ ci/build_windows.sh
@@ -2,13 +2,11 @@
 set -eux
 
 # Preflight checks
-make --version
-gcc --version
-
 export MSBUILD_DIR="C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/MSBuild/15.0/Bin/"
 export PATH="$PWD/cmake-3.12.0-rc1-win64-x64/bin:/mingw64/bin:$PATH"
 
-ls /mingw64/bin
+make --version
+gcc --version
 which gcc
 
 cd recast-wrapper
