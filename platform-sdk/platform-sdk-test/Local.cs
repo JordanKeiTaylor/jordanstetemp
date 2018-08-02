f8c3a1ca8d0318c94f928687ddaa699cc74d0eb0
Dennis Jordan <dennisjordan@improbable.io>
2018-08-02 01:38:38 -0600

    Update spatiald path

--- platform-sdk/platform-sdk-test/Local.cs
+++ platform-sdk/platform-sdk-test/Local.cs
@@ -17,7 +17,7 @@ namespace platform_sdk_test
         {
             var startInfo = new ProcessStartInfo
             {
-                FileName = Path.Combine(PlatformPath, "build/lib/spatiald"),
+                FileName = Path.Combine(PlatformPath, "lib/spatiald"),
                 Arguments = $"start " +
                             $"--port={Port} " +
                             $"--project_file={Path.Combine(ProjectPath, "spatialos.json")} " +
