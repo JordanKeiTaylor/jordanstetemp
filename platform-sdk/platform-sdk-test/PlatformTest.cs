ce31d3b9d975c89770b991d6879bdab67d68e15a
Dennis Jordan <dennisjordan@improbable.io>
2018-08-13 20:38:42 +0000

    Add snapshot service api end point tests

--- platform-sdk/platform-sdk-test/PlatformTest.cs
+++ platform-sdk/platform-sdk-test/PlatformTest.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text.RegularExpressions;
 using Improbable.SpatialOS.Deployment.V1Alpha1;
+using Improbable.SpatialOS.Snapshot.V1Alpha1;
 using NUnit.Framework;
 
 namespace platform_sdk_test
@@ -11,6 +14,7 @@ namespace platform_sdk_test
     {
         public PlatformTest()
         {
+            // TODO: Make these singletons for the test suite
             Local.Start();
             Platform.Setup();
         }
@@ -18,19 +22,20 @@ namespace platform_sdk_test
         [Test]
         public void Should_ListDeployments()
         {
-            var listRequest = new ListDeploymentsRequest
-            {
-                ProjectName = Platform.ProjectName,
-                DeploymentName = Platform.DeploymentName
-            };
             
-            var listResponse = Platform.LocalDeploymentServiceClient.ListDeployments(listRequest);
+            var deployments = Platform.LocalDeploymentServiceClient.ListDeployments(
+                new ListDeploymentsRequest
+                {
+                    ProjectName = Platform.ProjectName,
+                    DeploymentName = Platform.DeploymentName
+                }
+            );
             
-            Assert.AreEqual(1, listResponse.Count());
+            Assert.Greater(deployments.Count(), 0);
             
             Console.WriteLine("Deployment List Response");
             
-            foreach (var deployment in listResponse)
+            foreach (var deployment in deployments)
             {
                 Assert.AreEqual(Platform.ProjectName, deployment.ProjectName);
                 Assert.AreEqual(Platform.DeploymentName, deployment.Name);
@@ -47,19 +52,122 @@ namespace platform_sdk_test
             const string id = "0";
             const string projectName = "navmesh_walker";
             
-            var getDeployment = new GetDeploymentRequest()
-            {
-                Id = id,
-                ProjectName = projectName
-            };
-
-            var deployment = Platform.LocalDeploymentServiceClient.GetDeployment(getDeployment).Deployment;
+            var deployment = Platform.LocalDeploymentServiceClient.GetDeployment(
+                new GetDeploymentRequest
+                {
+                    Id = id,
+                    ProjectName = projectName
+                }
+            ).Deployment;
             
             Assert.NotNull(deployment);
             Assert.True(id.Equals(deployment.Id));
             Assert.True(projectName.Equals(deployment.ProjectName));
 
             Console.WriteLine($"Deployment {id}:\n{Regex.Replace(deployment.ToString(), @"\s+", "")}");
+        }
+        
+        // TODO: Move to another test class
+        [Test]
+        public static void Should_ListSnapshots()
+        {
+            var snapshots = Platform.LocalSnapshotServiceClient.ListSnapshots(
+                new ListSnapshotsRequest
+                {
+                    ProjectName = Platform.ProjectName,
+                    DeploymentName = Platform.DeploymentName
+                }
+            );
+            
+            Assert.Greater(snapshots.Count(), 0);
+            foreach (var snapshot in snapshots)
+            {
+                Assert.AreEqual(Platform.ProjectName, snapshot.ProjectName);
+                Assert.AreEqual(Platform.DeploymentName, snapshot.DeploymentName);
+            }
+        }
+
+        [Test]
+        public static void Should_GetSnapshot()
+        {
+            var id = "0";
+            var snapshot = Platform.LocalSnapshotServiceClient.GetSnapshot(
+                new GetSnapshotRequest
+                {
+                    Id = id
+                }
+            ).Snapshot;
+            
+            Assert.AreEqual(id, snapshot.Id);
+            Assert.Greater(snapshot.Size, 0);
+            Assert.AreEqual(Platform.DeploymentName, snapshot.DeploymentName);
+            Assert.AreEqual(Platform.ProjectName, snapshot.ProjectName);
+        }
+        
+        [Test]
+        public static void Should_TakeSnapshot()
+        {
+            var operation = Platform.LocalSnapshotServiceClient.TakeSnapshot(
+                new TakeSnapshotRequest
+                {
+                    Snapshot = new Snapshot
+                    {
+                        ProjectName = Platform.ProjectName,
+                        DeploymentName = Platform.DeploymentName
+                    }
+                }
+            );
+            operation.PollUntilCompleted();
+            var snapshot = operation.GetResultOrNull();
+            
+            Assert.IsNotEmpty(snapshot.Id);
+            Assert.Greater(snapshot.Size, 0);
+            Assert.AreEqual(Platform.DeploymentName, snapshot.DeploymentName);
+            Assert.AreEqual(Platform.ProjectName, snapshot.ProjectName);
+        }
+
+        [Test]
+        public static void Should_UploadSnapshot_And_ConfirmUpload()
+        {
+            var snapshotFile = "path_to_snapshot_file";
+            var snapshot = new Snapshot
+            {
+                ProjectName = Platform.ProjectName,
+                DeploymentName = Platform.DeploymentName
+            };
+
+            var uploadResponse = Platform.LocalSnapshotServiceClient.UploadSnapshot(
+                new UploadSnapshotRequest
+                {
+                    Snapshot = snapshot
+                }
+            );
+
+            var newSnapshot = uploadResponse.Snapshot;
+            var httpRequest = WebRequest.Create(uploadResponse.UploadUrl) as HttpWebRequest;
+            if (httpRequest != null)
+            {
+                httpRequest.Method = "PUT";
+                httpRequest.ContentLength = newSnapshot.Size;
+                httpRequest.Headers.Set("Content-MD5", newSnapshot.Checksum);
+                using (var dataStream = httpRequest.GetRequestStream())
+                {
+                    var bytes = File.ReadAllBytes(snapshotFile);
+                    dataStream.Write(bytes, 0, bytes.Length);
+                }
+                httpRequest.GetResponse();
+            }
+
+            var confirmResponse = Platform.LocalSnapshotServiceClient.ConfirmUpload(
+                new ConfirmUploadRequest
+                {
+                    Id = newSnapshot.Id,
+                    ProjectName = Platform.ProjectName,
+                    DeploymentName = Platform.DeploymentName
+                }
+            );
+            
+            Assert.IsNotNull(confirmResponse.Snapshot);
         }
 
         public void Dispose()
