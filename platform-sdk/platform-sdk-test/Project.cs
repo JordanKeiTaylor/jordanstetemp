f582274064251556fb36fefafaf6c01958ac5fee
Dennis Jordan <dennisjordan@improbable.io>
2018-08-29 16:06:00 +0100

    Remove hardcoded path from Project

--- platform-sdk/platform-sdk-test/Project.cs
+++ platform-sdk/platform-sdk-test/Project.cs
@@ -5,12 +5,12 @@ namespace platform_sdk_test
 {
     internal static class Project
     {
-        public static string Id => "2";
+        public static string Id => "0";
         
         public static string Name => "solutions";
 
         public static string DeploymentName => "ebu_starter_proj";
         
-        public static string LaunchConfigPath => Path.Combine(Utility.ProjectPath(), "../platform/local-cluster/2nodes.pb.json");
+        public static string LaunchConfigPath => Path.Combine(Utility.ProjectPath(), "<path-to-config>");
     }
 }
